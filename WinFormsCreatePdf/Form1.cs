b8cf1743f5aba196c7fc57bc2f3c2c0dacbd79ae
Erik Morén <erik.moren78@gmail.com>
2024-09-03 11:12:19 +0200

    Add project files.

---
+++ WinFormsCreatePdf/Form1.cs
@@ -0,0 +1,289 @@
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+using System.Reflection;
+
+namespace WinFormsCreatePdf
+{
+    public partial class Form1 : Form
+    {
+        public static QuestPDF.Infrastructure.Image LogoImage { get; } = QuestPDF.Infrastructure.Image.FromFile("logo.png");
+
+        public InvoiceModel Model { get; } = InvoiceDocumentDataSource.GetInvoiceDetails();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            QuestPDF.Settings.License = LicenseType.Community;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Document.Create(container =>
+            {
+                container.Page(page =>
+                {
+                    page.Size(PageSizes.A4);
+                    page.Margin(2, Unit.Centimetre);
+                    page.PageColor(Colors.White);
+                    page.DefaultTextStyle(x => x.FontSize(20));
+
+                    page.Header()
+                        .Text("Hello We Teach IT PDF!")
+                        .SemiBold().FontSize(36).FontColor(Colors.Blue.Medium);
+
+                    string text = "Här skriver vi blå text och under lite Lorem Ipsum drabel";
+                    page.Content()
+                        .PaddingVertical(1, Unit.Centimetre)
+                        .Column(x =>
+                        {
+                            x.Spacing(20);
+                            x.Item().Text(text).FontSize(20).SemiBold().FontColor(Colors.Blue.Medium);
+                            x.Item().Text(Placeholders.LoremIpsum());
+                            x.Item().Text("En logga måsta man ju har också. Kanske ska vara uppi i högra hörnet men här funkar det ju också").FontSize(10);
+                            x.Item().Image(LogoImage);
+                        });
+
+                    page.Footer()
+                        .AlignCenter()
+                        .Text(x =>
+                        {
+                            x.Span("Page ");
+                            x.CurrentPageNumber();
+                        });
+                });
+                container.Page(page =>
+                {
+                    page.Margin(50);
+
+                    page.Header().Element(ComposeHeader);
+                    page.Content().Element(ComposeContent);
+
+                    page.Footer().AlignCenter().Text(text =>
+                    {
+                        text.CurrentPageNumber();
+                        text.Span(" / ");
+                        text.TotalPages();
+                    });
+                });
+            }).GeneratePdf("hello world.pdf");
+        }
+
+        void ComposeHeader(IContainer container)
+        {
+            container.Row(row =>
+            {
+                row.RelativeItem().Column(column =>
+                {
+                    column
+                        .Item().Text($"Invoice #{Model.InvoiceNumber}")
+                        .FontSize(20).SemiBold().FontColor(Colors.Blue.Medium);
+
+                    column.Item().Text(text =>
+                    {
+                        text.Span("Issue date: ").SemiBold();
+                        text.Span($"{Model.IssueDate:d}");
+                    });
+
+                    column.Item().Text(text =>
+                    {
+                        text.Span("Due date: ").SemiBold();
+                        text.Span($"{Model.DueDate:d}");
+                    });
+                });
+
+                row.ConstantItem(175).Image(LogoImage);
+            });
+        }
+
+        void ComposeContent(IContainer container)
+        {
+            container.PaddingVertical(40).Column(column =>
+            {
+                column.Spacing(20);
+
+                column.Item().Row(row =>
+                {
+                    row.RelativeItem().Component(new AddressComponent("From", Model.SellerAddress));
+                    row.ConstantItem(50);
+                    row.RelativeItem().Component(new AddressComponent("For", Model.CustomerAddress));
+                });
+
+                column.Item().Element(ComposeTable);
+
+                var totalPrice = Model.Items.Sum(x => x.Price * x.Quantity);
+                column.Item().PaddingRight(5).AlignRight().Text($"Grand total: {totalPrice:C}").SemiBold();
+
+                if (!string.IsNullOrWhiteSpace(Model.Comments))
+                    column.Item().PaddingTop(25).Element(ComposeComments);
+            });
+        }
+
+        void ComposeTable(IContainer container)
+        {
+            var headerStyle = TextStyle.Default.SemiBold();
+
+            container.Table(table =>
+            {
+                table.ColumnsDefinition(columns =>
+                {
+                    columns.ConstantColumn(25);
+                    columns.RelativeColumn(3);
+                    columns.RelativeColumn();
+                    columns.RelativeColumn();
+                    columns.RelativeColumn();
+                });
+
+                table.Header(header =>
+                {
+                    header.Cell().Text("#");
+                    header.Cell().Text("Product").Style(headerStyle);
+                    header.Cell().AlignRight().Text("Unit price").Style(headerStyle);
+                    header.Cell().AlignRight().Text("Quantity").Style(headerStyle);
+                    header.Cell().AlignRight().Text("Total").Style(headerStyle);
+
+                    header.Cell().ColumnSpan(5).PaddingTop(5).BorderBottom(1).BorderColor(Colors.Black);
+                });
+
+                foreach (var item in Model.Items)
+                {
+                    var index = Model.Items.IndexOf(item) + 1;
+
+                    table.Cell().Element(CellStyle).Text($"{index}");
+                    table.Cell().Element(CellStyle).Text(item.Name);
+                    table.Cell().Element(CellStyle).AlignRight().Text($"{item.Price:C}");
+                    table.Cell().Element(CellStyle).AlignRight().Text($"{item.Quantity}");
+                    table.Cell().Element(CellStyle).AlignRight().Text($"{item.Price * item.Quantity:C}");
+
+                    static IContainer CellStyle(IContainer container) => container.BorderBottom(1).BorderColor(Colors.Grey.Lighten2).PaddingVertical(5);
+                }
+            });
+        }
+
+        void ComposeComments(IContainer container)
+        {
+            container.ShowEntire().Background(Colors.Grey.Lighten3).Padding(10).Column(column =>
+            {
+                column.Spacing(5);
+                column.Item().Text("Comments").FontSize(14).SemiBold();
+                column.Item().Text(Model.Comments);
+            });
+        }
+    }
+
+    public class AddressComponent : IComponent
+    {
+        private string Title { get; }
+        private Address Address { get; }
+
+        public AddressComponent(string title, Address address)
+        {
+            Title = title;
+            Address = address;
+        }
+
+        public void Compose(IContainer container)
+        {
+            container.ShowEntire().Column(column =>
+            {
+                column.Spacing(2);
+
+                column.Item().Text(Title).SemiBold();
+                column.Item().PaddingBottom(5).LineHorizontal(1);
+
+                column.Item().Text(Address.CompanyName);
+                column.Item().Text(Address.Street);
+                column.Item().Text($"{Address.City}, {Address.State}");
+                column.Item().Text(Address.Email);
+                column.Item().Text(Address.Phone);
+            });
+        }
+    }
+
+    public interface IDocument
+    {
+        DocumentMetadata GetMetadata();
+        DocumentSettings GetSettings();
+        void Compose(IDocumentContainer container);
+    }
+
+    public static class InvoiceDocumentDataSource
+    {
+        private static Random Random = new Random();
+
+        public static InvoiceModel GetInvoiceDetails()
+        {
+            var items = Enumerable
+                .Range(1, 25)
+                .Select(_ => GenerateRandomOrderItem())
+                .ToList();
+
+            return new InvoiceModel
+            {
+                InvoiceNumber = Random.Next(1_000, 10_000),
+                IssueDate = DateTime.Now,
+                DueDate = DateTime.Now + TimeSpan.FromDays(14),
+
+                SellerAddress = GenerateRandomAddress(),
+                CustomerAddress = GenerateRandomAddress(),
+
+                Items = items,
+                Comments = Placeholders.Paragraph()
+            };
+        }
+
+        private static OrderItem GenerateRandomOrderItem()
+        {
+            return new OrderItem
+            {
+                Name = Placeholders.Label(),
+                Price = (decimal)Math.Round(Random.NextDouble() * 100, 2),
+                Quantity = Random.Next(1, 10)
+            };
+        }
+
+        private static Address GenerateRandomAddress()
+        {
+            return new Address
+            {
+                CompanyName = Placeholders.Name(),
+                Street = Placeholders.Label(),
+                City = Placeholders.Label(),
+                State = Placeholders.Label(),
+                Email = Placeholders.Email(),
+                Phone = Placeholders.PhoneNumber()
+            };
+        }
+    }
+
+    public class InvoiceModel
+    {
+        public int InvoiceNumber { get; set; }
+        public DateTime IssueDate { get; set; }
+        public DateTime DueDate { get; set; }
+
+        public Address SellerAddress { get; set; }
+        public Address CustomerAddress { get; set; }
+
+        public List<OrderItem> Items { get; set; }
+        public string Comments { get; set; }
+    }
+
+    public class OrderItem
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    public class Address
+    {
+        public string CompanyName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public object Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
